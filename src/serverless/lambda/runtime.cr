e59d8de75b83fde53a4d239abdb6edc8bac91175
Andrew <awcrotwell@gmail.com>
2021-01-20 11:01:29 -0500

    Using crambda runtime

--- src/serverless/lambda/runtime.cr
+++ src/serverless/lambda/runtime.cr
@@ -1,88 +1,43 @@
-require "http"
-require "log"
-require "./http_request"
-require "./http_response"
+require "http/client"
+require "json"
 
-module SLS
-  module Lambda
-    class Runtime
-      HANDLER           = "_HANDLER"
-      TRACE_ID          = "_X_AMZN_TRACE_ID"
-      TRACE_ID_HEADER   = "Lambda-Runtime-Trace-Id"
-      REQUEST_ID_HEADER = "Lambda-Runtime-Aws-Request-Id"
-      RUNTIME_BASE_URL  = "/2018-06-01/runtime/invocation"
-      RUNTIME_API_URL   = RUNTIME_BASE_URL + "/next"
+module SLS::Lambda
+  def self.run_handler(handler : Proc(JSON::Any, Context, Object))
+    function_name = ENV["AWS_LAMBDA_RUNTIME_API"]
+    function_version = ENV["AWS_LAMBDA_FUNCTION_VERSION"]
+    memory_limit_in_mb = UInt32.new(ENV["AWS_LAMBDA_FUNCTION_MEMORY_SIZE"])
+    log_group_name = ENV["AWS_LAMBDA_LOG_GROUP_NAME"]
+    log_stream_name = ENV["AWS_LAMBDA_LOG_STREAM_NAME"]
+    host, port = ENV["AWS_LAMBDA_RUNTIME_API"].split(':')
 
-      getter host : String
-      getter port : Int16
-      getter handlers : Hash(String, (JSON::Any -> JSON::Any)) = Hash(String, (JSON::Any -> JSON::Any)).new
-      getter logger : ::Log
+    client = HTTP::Client.new(host, port)
 
-      def initialize(backend : ::Log::IOBackend? = nil, level = ::Log::Severity::Debug)
-        api = ENV["AWS_LAMBDA_RUNTIME_API"].split(":", 2)
-
-        @host = api[0]
-        @port = api[1].to_i16
-
-        # Format logs specifically for Lambda
-        backend ||= ::Log::IOBackend.new(formatter: Lambda.formatter)
-        Lambda::Log.setup do |c|
-          c.bind "serverless.lambda", level, backend
-        end
-        @logger = Lambda::Log.for("serverless.lambda")
+    while true
+      res = client.get("/2018-06-01/runtime/invocation/next")
+      if res.status_code != 200
+        raise "Unexpected response when invoking: #{res.status_code}"
       end
 
-      # Associate the block/proc to the function name
-      def register_handler(name : String, &handler : JSON::Any -> JSON::Any)
-        self.handlers[name] = handler
-      end
+      ENV["_X_AMZN_TRACE_ID"] = res.headers["Lambda-Runtime-Trace-Id"]? || ""
 
-      def run
-        loop do
-          process_handler
-        end
-      end
+      context = Context.new(
+        function_name,
+        function_version,
+        memory_limit_in_mb,
+        log_group_name,
+        log_stream_name,
+        res.headers["Lambda-Runtime-Aws-Request-Id"],
+        res.headers["Lambda-Runtime-Invoked-Function-Arn"],
+        Int64.new(res.headers["Lambda-Runtime-Deadline-Ms"]),
+        JSON.parse(res.headers["Lambda-Runtime-Cognito-Identity"]? || "null"),
+        JSON.parse(res.headers["Lambda-Runtime-Client-Context"]? || "null"),
+      )
 
-      def process_handler
-        handler_name = ENV[HANDLER]
+      result = handler.call(JSON.parse(res.body), context)
 
-        if handlers.has_key?(handler_name)
-          _process_request handlers[handler_name]
-        else
-          logger.error {
-            "unknown handler: #{handler_name}, available handlers: #{handlers.keys.join(", ")}"
-          }
-        end
-      end
-
-      def _process_request(proc : Proc(JSON::Any, JSON::Any))
-        client = HTTP::Client.new(host: @host, port: @port)
-
-        begin
-          response = client.get RUNTIME_API_URL
-          ENV[TRACE_ID] = response.headers[TRACE_ID_HEADER] || ""
-
-          aws_request_id = response.headers[REQUEST_ID_HEADER]
-          base_url = RUNTIME_BASE_URL + "/#{aws_request_id}"
-
-          input = JSON.parse response.body
-
-          body = proc.call input
-
-          logger.info { "preparing body #{body}" }
-          response = client.post("#{base_url}/response", body: body.to_json)
-          logger.debug { "response invocation response #{response.status_code} #{response.body}" }
-        rescue ex : Exception
-          body = %Q({ "statusCode": 500, "body" : "#{ex.message}" })
-          response = client.post("#{base_url}/error", body: body)
-
-          Log.error {
-            "response error invocation response from exception " \
-            "#{ex.message} #{response.status_code} #{response.body}"
-          }
-        ensure
-          client.close
-        end
+      res = client.post("/2018-06-01/runtime/invocation/#{context.aws_request_id}/response", body: result.to_json)
+      if res.status_code != 202
+        raise "Unexpected response when responding: #{res.status_code}"
       end
     end
   end
