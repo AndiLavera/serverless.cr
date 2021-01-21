a397cd602565b37a500a1af6230783136ad3472d
Andrew <awcrotwell@gmail.com>
2021-01-21 13:39:02 -0500

    Fix http res spec

--- spec/serverless/lambda/http_response_spec.cr
+++ spec/serverless/lambda/http_response_spec.cr
@@ -2,34 +2,45 @@ require "../../spec_helper"
 
 describe SLS::Lambda::HTTPResponse do
   it "always returns status code" do
-    json = JSON.parse SLS::Lambda::HTTPResponse.new(123).to_json
+    res = SLS::Lambda::HTTPResponse.new
+    res.status_code = 123
+
+    json = JSON.parse res.to_json
     json["statusCode"].as_i.should eq 123
-    json["body"]?.should be_nil
+    json["body"]?.should eq ""
   end
 
   it "can contain a body as text" do
+    res = SLS::Lambda::HTTPResponse.new
     text = "my text"
-    json = JSON.parse SLS::Lambda::HTTPResponse.new(200, text).to_json
+    res.body = text
+
+    json = JSON.parse res.to_json
     json["body"]?.should eq text
   end
 
   it "can contain a body as json" do
+    res = SLS::Lambda::HTTPResponse.new
     input = JSON.parse "{ \"foo\" : \"bar\" }"
-    json = JSON.parse SLS::Lambda::HTTPResponse.new(200, input).to_json
+    res.body = input
+
+    json = JSON.parse res.to_json
     json["body"]["foo"]?.should eq "bar"
   end
 
   it "can contain additional headers" do
-    response = SLS::Lambda::HTTPResponse.new(200, "body")
-    response.headers["Content-Type"] = "application/text"
-    json = JSON.parse response.to_json
+    res = SLS::Lambda::HTTPResponse.new
+    res.headers["Content-Type"] = "application/text"
+
+    json = JSON.parse res.to_json
     json["headers"]["Content-Type"]?.should eq "application/text"
   end
 
   it "can return a JSON::Any object" do
-    response = SLS::Lambda::HTTPResponse.new(200, "body")
-    response.headers["Content-Type"] = "application/text"
-    json = response.as_json
+    res = SLS::Lambda::HTTPResponse.new
+    res.headers["Content-Type"] = "application/text"
+    json = res.as_json
+
     json.should be_a(JSON::Any)
     json.as_h["headers"]["Content-Type"]?.should eq "application/text"
   end
